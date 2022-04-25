50ad18362227d20e938040030c27e865ab9def9d
ElshanAli <elshanml@code.edu.az>
2022-04-25 17:42:45 +0400

    Added task Student and Group number

---
+++ TaskStudentAndGroupclass/Group.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskStudentAndGroupclass
+{
+    class Group
+    {
+        public string GroupNo;
+        public byte GroupLimit;
+        Student[] _students;
+        public Group(string grno,byte limit)
+        {
+            GroupNo = grno;
+            GroupLimit = limit;
+            _students = new Student[0];
+        }
+        public bool AddStudent(Student student)
+        {
+            if (GroupLimit > _students.Length ) 
+            {
+                Array.Resize(ref _students, _students.Length + 1);
+                _students[_students.Length - 1] = student;
+
+                return true;
+
+            }
+            else
+            {
+                Console.WriteLine("This group is full");
+                return false;
+            }
+        }
+        public void GetStudent()
+        {
+            foreach (var student in _students)
+            {
+                Console.WriteLine($"Name: {student.Name} Surname: {student.Surname} Group Number: {GroupNo}");
+            }
+        }
+    }
+}
---
+++ TaskStudentAndGroupclass/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TaskStudentAndGroupclass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Group group = new Group("P324",4);
+            Console.WriteLine("Enter the Fullname");
+            Student student = new Student(Console.ReadLine(),Console.ReadLine());
+            group.AddStudent(student);
+            group.GetStudent();
+
+
+        }
+    }
+}
